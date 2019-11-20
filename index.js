756f36b2fec22494df88031431a3453804ceb86a
Alex Sodre <dev.sodre@lojacasasaoluiz.com.br>
2019-11-20 01:15:00 -0300

    chore: Altera retorno da rota

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express;
 
 app.get('/teste', (req, res) => {
-  return res.json({ Hello: 'World' })
+  return res.json({ Hello: 'World 2' })
 });
 
 app.listen(3333);
